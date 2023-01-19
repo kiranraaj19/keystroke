e6caad31c55a626d908d04c4b99823d636c3ea9a
kiranraaj <kiranraajsekar@gmail.com>
2023-01-19 19:14:18 +0530

    Added a listening to key events which leverages device_query crate and sends keycode in the mpsc channel for the main thread to listen to and forward it to frontend

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -9,6 +9,22 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+// Listen for key events using device_query library and when a key event occurs, send it on the mpsc channel
+use device_query::{DeviceEvents, DeviceState};
+use std::sync::{mpsc, Mutex};
+struct MpscChannel {
+    inner: Mutex<mpsc::Sender<String>>,
+}
+
+fn listen_for_key_events(sender: tauri::State<'_, MpscChannel>) {
+    let device_state = DeviceState::new();
+
+    let _guard = device_state.on_key_up(|key| {
+        let output_tx = *sender.inner.lock().unwrap();
+        output_tx.send(key.to_string());
+    });
+}
+
 fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
