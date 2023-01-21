7fd838b22a7c7c304e5fb900ad2f603610c3a5b9
kiranraaj <kiranraajsekar@gmail.com>
2023-01-21 19:34:19 +0530

    Modularized Titlebar Component

---
+++ src/lib/TitleBar.svelte
@@ -0,0 +1,38 @@
+<script>
+    //Titlebar events
+    import { appWindow } from "@tauri-apps/api/window";
+</script>
+
+<div class="buttons">
+    <button
+        class="button"
+        data-tauri-drag-region
+        on:click={async () => {
+            await appWindow.startDragging();
+        }}>/</button
+    >
+    <button
+        class="button"
+        on:click={() => {
+            appWindow.minimize();
+        }}
+    >
+        -
+    </button>
+    <button
+        class="button"
+        on:click={() => {
+            appWindow.close();
+        }}
+    >
+        X
+    </button>
+</div>
+
+<style>
+    .buttons {
+        position: absolute;
+        top: 0;
+        right: 0;
+    }
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,36 +1,9 @@
 <script>
-  import { TauriEvent } from "@tauri-apps/api/event";
   import "../app.css";
-
-  //Titlebar events
-  import { appWindow } from "@tauri-apps/api/window";
+  import TitleBar from "$lib/TitleBar.svelte";
 </script>
 
 <main class="container">
-  <div class="buttons">
-    <button
-      class="drag_button"
-      data-tauri-drag-region
-      on:click={async () => {
-        await appWindow.startDragging();
-      }}>/</button
-    >
-    <button
-      class="minimize_button"
-      on:click={() => {
-        appWindow.minimize();
-      }}
-    >
-      -
-    </button>
-    <button
-      class="close_button"
-      on:click={() => {
-        appWindow.close();
-      }}
-    >
-      X
-    </button>
-  </div>
+  <TitleBar />
   <slot />
 </main>
