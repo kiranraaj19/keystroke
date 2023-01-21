9da58d48a73c1cb092dbd05c614500903728ddf0
kiranraaj <kiranraajsekar@gmail.com>
2023-01-22 01:25:15 +0530

    Added a little push to the back of the queue animation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,41 @@
 <script>
   import { listen } from "@tauri-apps/api/event";
   import { onMount } from "svelte";
+  import { fade, fly } from "svelte/transition";
 
-  let key = " ";
-
+  /**
+   * @type {any[]}
+   */
+  let prev_keys = [];
+  let curr_key = "";
   onMount(async () => {
     const unlisten = await listen(
       "key_event",
       (/** @type {{ payload: any; }} */ event) => {
-        key = event.payload;
+        if (prev_keys.length > 1) {
+          prev_keys.shift();
+        }
+        prev_keys = [...prev_keys, curr_key];
+        curr_key = event.payload;
         // console.log(event);
       }
     );
   });
 </script>
 
-<p>{key}</p>
+<p>
+  {#each prev_keys as key}
+    <span in:fly={{ y: -5 }} out:fade>{key}</span>
+  {/each}
+  {#key curr_key}
+    <span in:fly={{ y: -5 }}>{curr_key}</span>
+  {/key}
+</p>
 
 <style>
   p {
     display: flex;
     align-items: center;
+    gap: 2px;
   }
 </style>
